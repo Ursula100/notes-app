52c84a7607bba4433627882729fbcbe39f098a8a
tamen <20100902@mail.wit.ie>
2023-03-16 13:40:18 +0000

    Add listing and counting of archived and active notes functionalities

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -35,4 +35,47 @@ class NoteAPI {
     fun isValidListIndex (index: Int, list: List<Any>): Boolean{
         return (index >= 0 && index < list.size)
     }
+
+    fun listActiveNotes(): String {
+        var listOfActiveNotes = ""
+        for (i in notes.indices) {
+            if(!notes[i].isNoteArchived)
+                listOfActiveNotes += "${i}: ${notes[i]} \n"
+        }
+        return if (listOfActiveNotes.isNullOrBlank()) {
+            "No currently active notes"
+        } else listOfActiveNotes
+    }
+
+    fun listArchivedNotes(): String {
+        var listOfArchivedNotes = ""
+        for (i in notes.indices) {
+            if(notes[i].isNoteArchived)
+                listOfArchivedNotes += "${i}: ${notes[i]} \n"
+        }
+        return if (listOfArchivedNotes.isNullOrBlank()) {
+            "No archived notes"
+        } else listOfArchivedNotes
+    }
+
+    fun numberOfArchivedNotes(): Int {
+        //helper method to determine how many archived notes there are
+        var numberOfArchivedNotes = 0
+        for (i in notes.indices) {
+            if(notes[i].isNoteArchived)
+                numberOfArchivedNotes += 1
+        }
+        return numberOfArchivedNotes
+
+    }
+
+    fun numberOfActiveNotes(): Int {
+        //helper method to determine how many active notes there are
+        var numberOfActiveNotes = 0
+        for (i in notes.indices) {
+            if(notes[i].isNoteArchived)
+                numberOfActiveNotes += 1
+        }
+        return numberOfActiveNotes
+    }
 }
